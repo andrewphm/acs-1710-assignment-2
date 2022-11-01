a4ec8fc08bd8d5ec171b20962d5f4edfe34e37b9
Andrew Pham <andrew.pham@shopify.com>
2022-11-01 16:54:43 -0400

    Add form to /froyo route and update results route

--- app.py
+++ app.py
@@ -3,49 +3,67 @@ import random
 
 app = Flask(__name__)
 
+
 def sort_letters(message):
     """A helper method to sort the characters of a string in alphabetical order
     and return the new string."""
-    return ''.join(sorted(list(message)))
+    return "".join(sorted(list(message)))
 
 
-@app.route('/')
+@app.route("/")
 def homepage():
     """A homepage with handy links for your convenience."""
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/froyo')
+
+@app.route("/froyo")
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br/>
+        <input type="text" name="flavor"><br/>
+        What toppings would you like?<br/> 
+        <input type="text" name="toppings"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
-@app.route('/froyo_results')
+
+@app.route("/froyo_results")
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    pass
+    users_froyo_flavor = request.args.get("flavor")
+    users_froyo_toppings = request.args.get("toppings")
+    return f"You ordered {users_froyo_flavor} flavored Fro-Yo with {users_froyo_toppings}!"
 
-@app.route('/favorites')
+
+@app.route("/favorites")
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
     pass
 
-@app.route('/favorites_results')
+
+@app.route("/favorites_results")
 def favorites_results():
     """Shows the user a nice message using their form results."""
     pass
 
-@app.route('/secret_message')
+
+@app.route("/secret_message")
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
     pass
 
-@app.route('/message_results', methods=['POST'])
+
+@app.route("/message_results", methods=["POST"])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
     pass
 
-@app.route('/calculator')
+
+@app.route("/calculator")
 def calculator():
     """Shows the user a form to enter 2 numbers and an operation."""
     return """
@@ -63,54 +81,58 @@ def calculator():
     </form>
     """
 
-@app.route('/calculator_results')
+
+@app.route("/calculator_results")
 def calculator_results():
     """Shows the user the result of their calculation."""
     pass
 
 
 HOROSCOPE_PERSONALITIES = {
-    'aries': 'Adventurous and energetic',
-    'taurus': 'Patient and reliable',
-    'gemini': 'Adaptable and versatile',
-    'cancer': 'Emotional and loving',
-    'leo': 'Generous and warmhearted',
-    'virgo': 'Modest and shy',
-    'libra': 'Easygoing and sociable',
-    'scorpio': 'Determined and forceful',
-    'sagittarius': 'Intellectual and philosophical',
-    'capricorn': 'Practical and prudent',
-    'aquarius': 'Friendly and humanitarian',
-    'pisces': 'Imaginative and sensitive'
+    "aries": "Adventurous and energetic",
+    "taurus": "Patient and reliable",
+    "gemini": "Adaptable and versatile",
+    "cancer": "Emotional and loving",
+    "leo": "Generous and warmhearted",
+    "virgo": "Modest and shy",
+    "libra": "Easygoing and sociable",
+    "scorpio": "Determined and forceful",
+    "sagittarius": "Intellectual and philosophical",
+    "capricorn": "Practical and prudent",
+    "aquarius": "Friendly and humanitarian",
+    "pisces": "Imaginative and sensitive",
 }
 
-@app.route('/horoscope')
+
+@app.route("/horoscope")
 def horoscope_form():
     """Shows the user a form to fill out to select their horoscope."""
-    return render_template('horoscope_form.html')
+    return render_template("horoscope_form.html")
 
-@app.route('/horoscope_results')
+
+@app.route("/horoscope_results")
 def horoscope_results():
     """Shows the user the result for their chosen horoscope."""
 
     # TODO: Get the sign the user entered in the form, based on their birthday
-    horoscope_sign = ''
+    horoscope_sign = ""
 
     # TODO: Look up the user's personality in the HOROSCOPE_PERSONALITIES
     # dictionary based on what the user entered
-    users_personality = ''
+    users_personality = ""
 
     # TODO: Generate a random number from 1 to 99
     lucky_number = 0
 
     context = {
-        'horoscope_sign': horoscope_sign,
-        'personality': users_personality, 
-        'lucky_number': lucky_number
+        "horoscope_sign": horoscope_sign,
+        "personality": users_personality,
+        "lucky_number": lucky_number,
     }
 
-    return render_template('horoscope_results.html', **context)
+    return render_template("horoscope_results.html", **context)
 
-if __name__ == '__main__':
-    app.config['ENV'] = 'development'
+
+if __name__ == "__main__":
+    app.config["ENV"] = "development"
     app.run(debug=True)
