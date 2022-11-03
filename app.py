ab1f04236a482394ab5a2f1b64441f5bb7f8b54b
Andrew Pham <andrew.pham@shopify.com>
2022-11-03 13:39:29 -0400

    Refactor calculator and Froyo to use render_template

--- app.py
+++ app.py
@@ -19,73 +19,110 @@ def homepage():
 @app.route("/froyo")
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    return """
-    <form action="/froyo_results" method="GET">
-        What is your favorite Fro-Yo flavor? <br/>
-        <input type="text" name="flavor"><br/>
-        What toppings would you like?<br/> 
-        <input type="text" name="toppings"><br/>
-        <input type="submit" value="Submit!">
-    </form>
-    """
+    return render_template("froyo_form.html")
 
 
 @app.route("/froyo_results")
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    users_froyo_flavor = request.args.get("flavor")
-    users_froyo_toppings = request.args.get("toppings")
-    return f"You ordered {users_froyo_flavor} flavored Fro-Yo with {users_froyo_toppings}!"
+
+    context = {
+        "users_froyo_flavor": request.args.get("flavor"),
+        "users_froyo_toppings": request.args.get("toppings"),
+    }
+    return render_template("froyo_results.html", **context)
 
 
 @app.route("/favorites")
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorites_results" method="GET">
+        <label>
+        What is your favorite color?
+        <input type="text" name="color" />
+        </label> </br>
+        <label>
+        What is your favorite animal?
+        <input type="text" name="animal" />
+        </label> </br>
+        <label>
+        What is your favorite city?
+        <input type="text" name="city" /></br>
+        <input type="submit" value="Submit!"/>
+        </label>
+    </form>
+
+    """
 
 
 @app.route("/favorites_results")
 def favorites_results():
     """Shows the user a nice message using their form results."""
-    pass
+    users_fav_color = request.args.get("color")
+    users_fav_animal = request.args.get("animal")
+    users_fav_city = request.args.get("city")
+
+    return f"<p>Wow, I didn't know {users_fav_color} {users_fav_animal}s lived in {users_fav_city}!</p>"
 
 
 @app.route("/secret_message")
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
-    pass
+
+    return """
+        <form action="/message_results" method="POST">
+            <label>
+                Enter a secret message:
+                <input type="text" name="message"/>
+            </label></br>
+
+            <input type="submit" value="Submit!"/>
+            
+        </form>
+    """
 
 
 @app.route("/message_results", methods=["POST"])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
-    pass
+    users_secret_message = request.form.get("message")
+    users_secret_message_sorted = sort_letters(users_secret_message)
+
+    return f"Here's your secret message!\n {users_secret_message_sorted}"
 
 
 @app.route("/calculator")
 def calculator():
     """Shows the user a form to enter 2 numbers and an operation."""
-    return """
-    <form action="/calculator_results" method="GET">
-        Please enter 2 numbers and select an operator.<br/><br/>
-        <input type="number" name="operand1">
-        <select name="operation">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select>
-        <input type="number" name="operand2">
-        <input type="submit" value="Submit!">
-    </form>
-    """
+    return render_template("calculator_form.html")
 
 
 @app.route("/calculator_results")
 def calculator_results():
     """Shows the user the result of their calculation."""
-    pass
+    operation = request.args.get("operation")
+    operand1 = int(request.args.get("operand1"))
+    operand2 = int(request.args.get("operand2"))
+
+    if operation == "add":
+        result = operand1 + operand2
+    elif operation == "subtract":
+        result = operand1 - operand2
+    elif operation == "multiply":
+        result = operand1 * operand2
+    elif operation == "divide":
+        result = operand1 / operand2
+
+    context = {
+        "operation": operation,
+        "operand1": operand1,
+        "operand2": operand2,
+        "result": result,
+    }
+
+    return render_template("calculator_results.html", **context)
 
 
 HOROSCOPE_PERSONALITIES = {
